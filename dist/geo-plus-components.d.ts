0700f10b1cce282d781cf570ee72c698467cf72d
Andrey Ryabinin <an.sorbier@gmail.com>
2017-12-15 12:20:03 -0500

    css

--- dist/geo-plus-components.d.ts
+++
@@ -1,6 +0,0 @@
-/**
- * Generated bundle index. Do not edit.
- */
-export * from './public_api';
-export { GpcNavbarItemComponent as ɵb } from './src/app/modules/gpc-navbar/gpc-navbar-item/gpc-navbar-item.component';
-export { GpcNavbarComponent as ɵa } from './src/app/modules/gpc-navbar/gpc-navbar.component';
--- dist/src/app/modules/gpc-navbar/gpc-navbar-item/gpc-navbar-item.component.d.ts
+++
@@ -1,5 +0,0 @@
-export declare class GpcNavbarItemComponent {
-    items: any;
-    level: number;
-    constructor();
-}
